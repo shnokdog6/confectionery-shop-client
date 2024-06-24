dfbf9e1b7c9bc213a03553903c3e4223eb16826a
beyondray <shnokdog123@gmail.com>
2024-06-24 10:30:50 +0500

    fix: add re-expoert of useProductQuery

--- src/entities/product/index.ts
+++ src/entities/product/index.ts
@@ -1,3 +1,3 @@
 export { IProduct } from "./model/IProduct";
 export { ProductCard, ProductCardProps } from "./ui/product-card/ProductCard";
-export { useProductsQuery } from "./api/productApi";
+export { useProductsQuery, useProductQuery } from "./api/productApi";
